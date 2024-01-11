152dc5ea94c0345d79d66bbf8e65e97d77ebe83d
matildes <matildes@student.21-school.ru>
2024-01-11 11:53:33 +0500

    baners

---
+++ banners/gamebanner.c
@@ -0,0 +1,8 @@
+// library_name.c
+
+#include <stdio.h>
+#include "gamebanner.h"
+
+void printSalom() {
+    printf("Salom BXcloner\n");
+}
---
+++ banners/gamebanner.h
@@ -0,0 +1,6 @@
+#ifndef GAMEBANNER_H
+#define GAMEBANNER_H
+
+void printSalom();
+
+#endif // LIBRARY_NAME_H
